370b09ebb6ed2cf5f06d71cc212a90a62f46a1d5
carlosherre <acmontero@uninorte.edu.co>
2021-12-03 13:55:38 -0500

    Se agregó un archivo enVivo.json para ver partidos en linea

--- src/components/contentHome.jsx
+++ src/components/contentHome.jsx
@@ -1,90 +1,36 @@
 import React, { Fragment } from "react";
+import enVivo from "../enVivo.json";
 
 export function ContentHome(){
-    
+   const partidos = enVivo;
+  
+   
+       
+   
     return (
         <Fragment>
-            <div className=" position-static d-flex bg-secondary py-4 col " tabindex="-1" role="dialog" id="modalSignin">
+            <div className="container position-static d-flex bg-secondary py-4 col " tabIndex="-1" role="dialog" id="modalSignin">
                 
-            <table className="table table-dark table-bordered table-striped table-hover justify-content-md-center">
+            <table className="table table-dark table-borderless table-striped table-hover">
                 <thead>
                     <tr className="bg-danger.bg-gradient">
-                       <td colSpan="4" className="bg-warning h1 text-center text-black"> Partidos en Vivo</td>
+                       <td colSpan="3" className="bg-warning h1 text-center text-black"> Partidos en Vivo</td>
                     </tr>
                     <tr className="bg-black">
-                        <th className="bg-black" scope="col">#</th>
-                        <th className="bg-black" scope="col">Local</th>
-                        <th className="bg-black" scope="col">Visitante</th>
-                        <th className="bg-black" scope="col">Marcador</th>
+                        <th className="bg-black text-center" scope="col">Local</th>
+                        <th className="bg-black text-center" scope="col">Marcador</th>
+                        <th className="bg-black text-center" scope="col">Visitante</th>
+                        
                     </tr>
                 </thead>
-                <tbody>
+                <tbody >
+                  {partidos.map(partido => 
                     <tr>
-                        <th scope="row">1</th>
-                        <td>Real Madrid FC</td>
-                        <td>Barcelona FC</td>
-                        <td>0 - 0</td>
+                        <td className="text-center">{partido.local}</td>
+                        <td className="text-center">{partido.resultado}</td>
+                        <td className="text-center">{partido.visitante}</td>
                     </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Manchester United</td>
-                        <td>Manchester City</td>
-                        <td>2 - 1</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Bayern Munchen</td>
-                        <td>Schalke 04</td>
-                        <td>3 - 3</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">4</th>
-                        <td>Atletico Nacional</td>
-                        <td>Atletico Junior</td>
-                        <td>1 - 2</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">5</th>
-                        <td>AS Mónaco</td>
-                        <td>Paris Saint Germain</td>
-                        <td>1 - 0</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">6</th>
-                        <td>Flamenco</td>
-                        <td>Palmeiras</td>
-                        <td>1 - 1</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">7</th>
-                        <td>Boca Juniors</td>
-                        <td>River Plate</td>
-                        <td>0 - 0</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">8</th>
-                        <td>Chelsea</td>
-                        <td>Juventus</td>
-                        <td>4 - 0</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">8</th>
-                        <td>Rayo Vallecano</td>
-                        <td>Valencia</td>
-                        <td>2 - 0</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">8</th>
-                        <td>Inter de Milán</td>
-                        <td>AC Milán</td>
-                        <td>1 - 2</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">8</th>
-                        <td>Millonarios</td>
-                        <td>Santa fé</td>
-                        <td>1 - 0</td>
-                    </tr>
+                  )}
                 </tbody>
                 
             </table>
